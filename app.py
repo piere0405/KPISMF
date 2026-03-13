db07a6aef57ff4841cb50c5d62485263b8f2cbec
piere0405 <jampierlml@gmail.com>
2026-03-13 15:40:31 -0500

    pd

--- app.py
+++ app.py
@@ -215,5 +215,5 @@ with tab3:
         if plaza :
             datos = df_precal[df_precal["Supervisor"].isin(plaza)]
             riveros = st.selectbox("Elije jerarquia",jerarquia)
-            tabla = st.crosstab(datos["DIA"],datos[jerarquia])
+            tabla = pd.crosstab(datos["DIA"],datos[jerarquia])
             st.dataframe(tabla)

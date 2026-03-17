9c3f62febf0e59c08662ec82b1ab76d0a0f8c500
piere0405 <jampierlml@gmail.com>
2026-03-17 14:31:29 -0500

    hojas

--- app.py
+++ app.py
@@ -201,6 +201,10 @@ with tab2:
     ventas = st.file_uploader("Sube el drive de ventas",type=[".xlsx"])
     if ventas is None :
         st.warning("Debas cargar tu archivo para que tengas tu reporte")
+    else :
+        df_ventas = pd.read_excel(ventas)
+        hojas = df_ventas.sheet_names
+        plazas = st.selectbox("ELIJE PLAZA : " ,hojas)    
 with tab3:
     precales = st.file_uploader("Sube el archivo de precales",type=[".xlsx"])
     if precales is None :
